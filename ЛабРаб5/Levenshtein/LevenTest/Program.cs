03d57bf305d9da385289853a80056693716fe6bd
MariaLaricheva <43138491+MariaLaricheva@users.noreply.github.com>
2019-01-22 23:37:56 +0300

    Add files via upload

---
+++ ЛабРаб5/Levenshtein/LevenTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LevenTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Levenshtein.Levenshtein.WriteDistance("Москва", "Братва");
+        }
+    }
+}
